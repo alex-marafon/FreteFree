365d30b0959c496c3dc81ba6d557334c49bc3bf2
Alex Marafon <alex.marafonn@gmail.com>
2020-12-15 01:08:28 -0400

    Ajustes de Layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Logging;
 using FreteFree.Models;
 using FreteFree.Data;
 using Microsoft.EntityFrameworkCore;
+using PdfSharpCore.Drawing;
+using System.IO;
 
 namespace FreteFree.Controllers
 {
@@ -36,6 +38,52 @@ namespace FreteFree.Controllers
         }
 
 
+        public FileResult GerarPDF()
+        {
+            using (var doc = new PdfSharpCore.Pdf.PdfDocument()) 
+            {
+                var page = doc.AddPage();
+                page.Size = PdfSharpCore.PageSize.A4;
+                page.Orientation = PdfSharpCore.PageOrientation.Portrait;
+
+                var graphics = PdfSharpCore.Drawing.XGraphics.FromPdfPage(page);
+                var corFonte = PdfSharpCore.Drawing.XBrushes.Black;
+
+                var textFormatter = new PdfSharpCore.Drawing.Layout.XTextFormatter(graphics);
+                var fonteOrganzacao = new PdfSharpCore.Drawing.XFont("Arial", 10);
+                var fonteDescricao = new PdfSharpCore.Drawing.XFont("Arial", 8, PdfSharpCore.Drawing.XFontStyle.BoldItalic);
+                var titulodetalhes = new PdfSharpCore.Drawing.XFont("Arial", 14, PdfSharpCore.Drawing.XFontStyle.Bold);
+                var fonteDetalhesDescricao = new PdfSharpCore.Drawing.XFont("Arial", 7);
+
+                var logo = @"C:\Users\Trut4\source\repos\FreteFree\wwwroot\imagens\Capturar.JPG";
+
+                var artPaginas = doc.PageCount;
+
+                var qtdPaginas = doc.PageCount;
+                textFormatter.DrawString(qtdPaginas.ToString(), new PdfSharpCore.Drawing.XFont("Arial", 10), corFonte, new PdfSharpCore.Drawing.XRect(578, 825, page.Width, page.Height));
+
+                XImage imagem = XImage.FromFile(logo);
+                graphics.DrawImage(imagem, 20, 5, 300, 50);
+
+
+
+
+
+
+
+
+
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    var contantType = "application/pdf";
+                    doc.Save(stream, false);
+
+                    var nomeArquivo = "RelatorioValdir.pdf";
+
+                    return File(stream.ToArray(), contantType, nomeArquivo);
+                }
+            }
+        }
 
 
         //pesquisa SQL Like   nao funciona com class agrupadas pois o valor de retorno da colsulta é o ID e nao a string pesquisada
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//<script>
+//    function myFunction() {
+//        var input, filter, table, mytbody, tr, td, i, txtValue;
+//        input = document.getElementById("myInput");
+//        filter = input.value.toUpperCase();
+//        table = document.getElementById("myTable");
+//        tbody = document.getElementById("myTbody");
+//        tr = table.getElementsByTagName("tr");
+//        for (i = 0; i < tr.length; {
+//        td = tr[i].getElementsByTagName("td")[0];
+//            if (td) {
+//        txtValue = td.textContent || td.innerText;
+//                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//        tr[i].style.display = "";
+//                } else {
+//        tr[i].style.display = "none";
+//                };
+//            };
+//        };
+//    };
+//</script>
+
+
+
+//function myFunction() {
+//    var input, filter, table, tbody, tr, td, i, txtValue;
+//    input = document.getElementById("myInput");
+//    filter = input.value.toUpperCase();
+//    table = document.getElementById("search");
+//    tbody = document.getElementById("sear")
+//    tr = tbody.getElementsByTagName("tr");
+//    for (i = 0; i < tr.length; i++) {
+//        td = tr[i].getElementsByTagName("td")[0];
+//        if (td) {
+//            txtValue = td.textContent || td.innerText;
+//            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//                tr[i].style.display = "";
+//            } else {
+//                tr[i].style.display = "none";
+//            }
+//        }
+//    }
+//}
